84bd4abf3135c62f7b15113a154017ef3171251f
Gautam Jain <88053172+BloodThearch@users.noreply.github.com>
2023-02-24 09:51:36 +0530

    Update glut_test1.cpp

--- glut_test1.cpp
+++ glut_test1.cpp
@@ -12,6 +12,67 @@ void myinit()
 	//glViewport(0, 0, 500, 500);
 }
 
+void setPixel(GLint x, GLint y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+}
+
+void ellipseMidPoint(int rx, int ry, int xCenter, int yCenter)
+{
+	//plotting first region	
+	float x = 0;
+	float y = ry;
+	float p1 = ry * ry - (rx * rx) * ry + (rx * rx) * (0.25);
+	//slope
+	float dx = 2 * (ry * ry) * x;
+	float dy = 2 * (rx * rx) * y;
+	while (dx < dy)
+	{
+		setPixel(xCenter + x, yCenter + y);
+		setPixel(xCenter - x, yCenter + y);
+		setPixel(xCenter + x, yCenter - y);
+		setPixel(xCenter - x, yCenter - y);
+		if (p1 < 0)
+		{
+			x = x + 1;
+			dx = 2 * (ry * ry) * x;
+			p1 = p1 + dx + (ry * ry);
+		}
+		else {
+			x = x + 1;
+			y = y - 1;
+			dx = 2 * (ry * ry) * x;
+			dy = 2 * (rx * rx) * y;
+			p1 = p1 + dx - dy + (ry * ry);
+		}
+	}
+	//plotting region 2
+	float p2 = (ry * ry) * (x + 0.5) * (x + 0.5) + (rx * rx) * (y - 1) * (y - 1) - (rx * rx) * (ry * ry);
+	while (y > 0)
+	{
+		setPixel(xCenter + x, yCenter + y);
+		setPixel(xCenter - x, yCenter + y);
+		setPixel(xCenter + x, yCenter - y);
+		setPixel(xCenter - x, yCenter - y);
+		if (p2 > 0)
+		{
+			x = x;
+			y = y - 1;
+			dy = 2 * (rx * rx) * y;
+			p2 = p2 - dy + (rx * rx);
+		}
+		else {
+			x = x + 1;
+			y = y - 1;
+			dy = dy - 2 * (rx * rx);
+			dx = dx + 2 * (ry * ry);
+			p2 = p2 + dx - dy + (rx * rx);
+		}
+	}
+}
+
 void bcircle(int xin, int yin, int rin)
 {
 //	glColor3f(1, 0, 0);
@@ -194,7 +255,7 @@ void display()
 //	ddaLine(0, -200, 100, 0);
 //	bline(0, -200, 100, 0);
 	
-	
+	ellipseMidPoint(65, 35, 0, 0);
 	
 	glFlush();
 }
@@ -211,4 +272,3 @@ int main(int argc, char** argv)
 	glutDisplayFunc(display);
 	glutMainLoop();
 }
-
